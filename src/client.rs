2be3111e1bf6d35e94342c0690d7533bba5a55c6
Nell Fauveau <nellousan@hotmail.fr>
2023-04-07 09:36:17 +0200

    Request handling architecture

--- src/client.rs
+++ src/client.rs
@@ -1,3 +1,4 @@
+use rtsp_types::{Method, Request};
 use std::sync::Arc;
 use tokio::io::AsyncReadExt;
 use tokio::net::TcpStream;
@@ -30,7 +31,7 @@ pub async fn handle_client(client: ClientRef) -> () {
             .tcp_socket
             .read(&mut buf)
             .await
-            .expect("lol");
+            .expect("failed to read on socket.");
 
         println!("read:\n{}", String::from_utf8_lossy(buf.as_slice()));
 
@@ -38,19 +39,23 @@ pub async fn handle_client(client: ClientRef) -> () {
             return;
         }
 
-        let (message, _): (rtsp_types::Message<Vec<u8>>, _) =
-            rtsp_types::Message::parse(&buf).expect("Failed to parse rtsp");
-
-        if let rtsp_types::Message::Request(request) = message {
-            println!("message type: {:?}", request);
-        }
-
-        // match message {
-        //     rtsp_types::Message::Request(ref request) => {
-        //         println!("message type: {:?}", request.method());
-        //     }
-        //     _ => unreachable!(),
-        // }
-        // socket.write_all(&buf[0..n]).await.expect("mdr");
+        handle_message(client.clone(), &buf).await;
     }
+}
+
+pub async fn handle_message(client: ClientRef, data: &Vec<u8>) -> () {
+    let (message, _): (rtsp_types::Message<Vec<u8>>, _) =
+        rtsp_types::Message::parse(&data).expect("Failed to parse rtsp");
+
+    if let rtsp_types::Message::Request(request) = message {
+        println!("message type: {:?}", request);
+        match request.method() {
+            Method::Setup => handle_setup(client, request).await,
+            _ => println!("unhandled request method: {:?}", request.method()),
+        }
+    }
+}
+
+pub async fn handle_setup(client: ClientRef, request: Request<Vec<u8>>) -> () {
+    println!("got setup")
 }
